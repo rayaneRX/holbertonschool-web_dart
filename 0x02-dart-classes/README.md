54f3dcbb81cdc5b8d5bc7425e2dadafdeba905fa
Rayane El kefif <113839670+rayaneRX@users.noreply.github.com>
2024-09-01 18:59:55 +0000

    push all dart project

---
+++ 0x02-dart-classes/README.md
@@ -0,0 +1,7 @@
+# 0x02-dart-classesclass User {
+    String? name;
+
+    String showName() {
+    return 'Hello ${name}';
+  }
+}
---
+++ 0x03-asynchronous/0-users_count.dart
@@ -0,0 +1,11 @@
+
+import '0-util.dart';
+
+Future<void> usersCount() async {
+  try {
+    int count = await fetchUsersCount();
+    print(count);
+  } catch (e) {
+    print('Error fetching user count: $e');
+  }
+}
